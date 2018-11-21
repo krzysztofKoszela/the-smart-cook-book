60c087908143b6038295999648c4e27b35bec793
krzysztofKoszela <krzysztof.koszela96@gmail.com>
2018-11-21 01:29:07 +0100

    update multiselect component

--- theSmartCookBook/src/components/search.vue
+++ theSmartCookBook/src/components/search.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <div class="srch">
-  <multiselect v-model="value" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="code" :options="options" :multiple="true" :taggable="true" @tag="addTag"></multiselect>
+  <div>
+  <multiselect class="multi-search" v-model="value" tag-placeholder="Add this as new tag" placeholder="Search or add a tag" label="name" track-by="code" :options="options" :multiple="true" :taggable="true" @tag="addTag"></multiselect>
   <pre class="language-json"><code>{{ value  }}</code></pre>
 </div>
 
@@ -41,6 +41,10 @@ export default {
 
 }
 </script>
-
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<style>
+.multi-search{
+  width: 50%;
+}
+</style>
 
